4506ea12b0d6e9387910ee9ed372b744f24a398a
inaaaas <ina_hovhannisyan04@mail.ru>
2025-03-19 17:22:27 +0400

    done

---
+++ BankAccount.cpp
@@ -0,0 +1,56 @@
+#include "BankAccount.h"
+#include <iostream>
+
+BankAccount::BankAccount() {
+    this -> m_accountHolder = "Unknown";
+    this -> m_accountNumber = 0;
+    this -> m_balance = 0.0;
+}
+/*
+BankAccount::BankAccount() : m_accountHolder("Unknown"), m_accountNumber(0), m_balance(0.0) {}
+*/
+BankAccount::BankAccount(const std::string& accountHolder, int accountNumber, double balance) {
+    this -> m_accountHolder = accountHolder;
+    this -> m_accountNumber = accountNumber;
+    this -> m_balance = balance;
+}
+BankAccount::BankAccount(const BankAccount& other) {
+    this -> m_accountHolder = other.m_accountHolder;
+    this -> m_accountNumber = other.m_accountNumber;
+    this -> m_balance = other.m_balance;
+}
+BankAccount& BankAccount::operator=(const BankAccount& other) {
+    if (this == &other) {
+        return *this;
+    }
+    this -> m_accountHolder = other.m_accountHolder;
+    this -> m_accountNumber = other.m_accountNumber;
+    this -> m_balance = other.m_balance;
+    return *this;
+}
+BankAccount::~BankAccount() {
+    std::cout << "Account number " << m_accountHolder << "is destroyed." << std::endl;
+}
+
+void BankAccount::deposit(double amount) {
+    if (amount > 0) {
+        m_balance += amount;
+    }
+}
+bool BankAccount::withdraw(double amount) {
+    if (m_balance >= amount && amount > 0) {
+        m_balance -= amount;
+        return true;
+    }
+    std::cout << "Error: not enough money" << std::endl;
+    return false;
+
+}
+void BankAccount::display() const {
+    std::cout << "Account Holder: " << m_accountHolder 
+    << ", Account Number:  " << m_accountNumber 
+    << ", Account balance: " << m_balance << std::endl;
+}
+int BankAccount::getAccountNumber() const {
+    return this -> m_accountNumber;
+}
